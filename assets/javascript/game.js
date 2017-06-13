bb5bbbb472cd8f9c5d5005fcbc1468685e902ba6
Robert Diluzio Zobrist <zobrist.robert.480@gmail.com>
2017-06-13 12:32:22 -0700

    attempting to fix the restart portion of game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,25 +6,31 @@ var wordInQuestion = wordBank[Math.floor(Math.random() * wordBank.length)];
 var gameOver = true;
 var gameLetters = [];
 var offLimitLetters = [];
+var offLimitWords = [];
 var wins = 0;
+var losses = 0;
 var guessesLeft = 6;
 
+
+function anoherWord() {
+     wordInQuestion = wordBank[Math.floor(Math.random() * wordBank.length)];
+}
 
 
 // checking to see if user guessed all letters correctly \\
 function didUserWin() {
-    for (var i = 0; i < gameLetters.length; i++) {
-        if (gameLetters[i] === "_") {
-            return false; // User still has letters to guess \\
-        }
-    }
-    return true; // All letters have been guessed \\
+     for (var i = 0; i < gameLetters.length; i++) {
+          if (gameLetters[i] === "_") {
+               return false; // User still has letters to guess \\
+          }
+     }
+     return true; // All letters have been guessed \\
 }
 
 
-
+// displays underscores on html, each one reresenting an unguessed letter\\
 for (var i = 0; i < wordInQuestion.length; i++) {
-    gameLetters.push("_");
+     gameLetters.push("_");
 
 }
 
@@ -32,103 +38,121 @@ for (var i = 0; i < wordInQuestion.length; i++) {
 
 // image swap function in coorrelation with user incorrect guesses \\
 function killingJimmy() {
-    document.getElementById("fateofJimmy").src = "assets/images/hangmanImg" + guessesLeft + ".png";
+     document.getElementById("fateofJimmy").src = "assets/images/hangmanImg" + guessesLeft + ".png";
 }
 
 // Resets game by refreshing page
 function resetGame() {
-    window.location.reload();
+     gameLetters = [];
+     offLimitLetters = [];
+     guessesLeft = 7;
+     console.log(wins);
+     console.log(losses);
 }
 // appends a space in between every character \\
 function spacedOut(arr) {
-    var result = "";
+     var result = "";
 
-    for (var i = 0; i < arr.length; i++) {
-        result += arr[i];
+     for (var i = 0; i < arr.length; i++) {
+          result += arr[i];
 
-        if (i != arr.length - 1) {
-            result += " ";
-        }
-    }
+          if (i != arr.length - 1) {
+               result += " ";
+          }
+     }
 
-    return result;
+     return result;
 }
 
 // displaying the word player will guess, undercores on screen till user chooses correctly \\
 function frontAndcenter() {
-    document.getElementById("stageWord").innerHTML = "Your Word: " + spacedOut(gameLetters);
+     document.getElementById("stageWord").innerHTML = "Your Word: " + spacedOut(gameLetters);
 
 }
 // displaying letters already guessed incorrectly \\
 function eightysixdLetters() {
-    document.getElementById("offLimitLetters").innerHTML = "Letter Cemetary: " + spacedOut(offLimitLetters);
+     document.getElementById("offLimitLetters").innerHTML = "Letter Cemetary: " + spacedOut(offLimitLetters);
 }
 // //logic of gameplay \\\
 
 
-    alert("Go ahead and take your first guess to begin!");
-    document.onkeyup = function(key) {
-// Checking to see if user has already won without leaving the app \\
-if((gameOver === true) && (wins > 0)){
-document.getElementById("results").innerHTML = "Wins: " + wins;
-}       else //// captures user guess \\\\
-        {
-            console.log(wordInQuestion);
-            var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-            if (alphabet.indexOf(userGuess) >= 0) {
-                gameOver = false;
-                
 
-            } else if (alphabet.indexOf(userGuess) === -1) {
-                alert("please press a letter!"); 
-
-            }if(gameOver === false){
-    if ((gameOver === true) && (wins > 0)) {
-        document.getElementById("results").innerHTML = "Wins: " + wins;
-    } else //// captures user guess \\\\
-    {
-        console.log(wordInQuestion);
-        
-        for (var i = 0; i < wordInQuestion.length; i++) {
-            if (wordInQuestion.charAt(i) === userGuess) {
-                gameLetters[i] = String(userGuess);
-            }
-        }
-        frontAndcenter();
-
-        // check to see if the user has guessed all the letters correctly \\
-        if (didUserWin()) {
-            document.getElementById("results").innerHTML = "Congratulations! You win!";
-            resetGame();
-            wins++;
-        } else {
-            if (offLimitLetters.indexOf(userGuess) === -1) {
-                if (gameLetters.indexOf(userGuess) === -1) {
+document.onkeyup = function(key) {
+     // Checking to see if user has already won without leaving the app \\
+     if ((gameOver === true) && (wins > 0)) {
+          document.getElementById("results").innerHTML = "Wins: " + wins;
+     } else //// captures user guess \\\\
+     {
+          console.log(wordInQuestion);
+          var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+          if (alphabet.indexOf(userGuess) >= 0) {
+               gameOver = false;
 
 
-                 guessesLeft--;
-                  killingJimmy();
+          } else if (alphabet.indexOf(userGuess) === -1) {
+               document.getElementById('message').innerHTML = "Please select a letter!";
+
+          }
+          if (gameOver === false) {
+               if ((gameOver === true) && (wins > 0)) {
+                    document.getElementById("results").innerHTML = "Wins: " + wins;
+               } else //// captures user guess \\\\
+               {
 
 
-                    // Add the guessed letter to the usedLetters array
-                    offLimitLetters.push(userGuess);
-                    eightysixdLetters();
-                }
-            }
+                    for (var i = 0; i < wordInQuestion.length; i++) {
+                         if (wordInQuestion.indexOf(i) === userGuess) {
+                              gameLetters[i] = String(userGuess);
+                         }
+                    }
+                    frontAndcenter();
+
+                    // check to see if the user has guessed all the letters correctly \\
+                    if (didUserWin()) {
+                         wordBank.splice(wordInQuestion);
+                         offLimitWords.push(wordInQuestion);
+                         gameOver = true;
+                         document.getElementById("results").innerHTML = "Congratulations! You win!";
+                         resetGame();
+                         console.log(offLimitLetters);
+                         guessesLeft = 6;
+                         killingJimmy();
+                         anoherWord();
+                         wins++;
+                    } else {
+                         if (offLimitLetters.indexOf(userGuess) === -1) {
+                              if (gameLetters.indexOf(userGuess) === -1) {
 
 
-            // Check if player lost
-            if (guessesLeft === 0) {
-                // Player loses
-                document.getElementById("message").innerHTML = "You Lose!";
-   
-                appRunning = false;
-
-               
-            }
-        }
+                                   guessesLeft--;
+                                   killingJimmy();
 
 
-    }
-}
-        }};
+                                   // Add the guessed letter to the usedLetters array
+                                   offLimitLetters.push(userGuess);
+                                   eightysixdLetters();
+                              }
+                         }
+
+
+                         // Check if player lost
+                         if (guessesLeft === 0) {
+                              losses++;
+                              gameOver = true;
+                              // Player loses
+                              resetGame();
+                              guessesLeft = 7;
+                              killingJimmy();
+                              anoherWord();
+                              document.getElementById("message").innerHTML = "You Lose!";
+
+
+
+                         }
+                    }
+
+
+               }
+          }
+     }
+};
