1538c7bcebb681e1813c96c3fcdfdaae7cd76e00
Robert Diluzio Zobrist <zobrist.robert.480@gmail.com>
2017-06-14 12:10:34 -0700

    changes

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var http = require('http');
 
 http.createServer(function(request, response){
 	response.writeHead(200, {"Content-Type":"text/plain"})
-	response.get('index.html');
+	
 }).listen(process.env.PORT)
